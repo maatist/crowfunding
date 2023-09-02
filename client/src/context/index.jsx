9d96444e0896bdde99fa981d6c84591e97c0e273
Matias Sepulveda <matias.seto@gmail.com>
2023-09-01 22:31:05 -0400

    Fix args en createCampaign

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -14,14 +14,16 @@ export const StateContextProvider = ({ children }) => {
     const publishCampaign = async (form) => {
 
         try {
-            const data = await createCampaign([
-                address, // dueño de la campaña
-                form.title, // titulo de la campaña
-                form.description, // descripcion de la campaña
-                form.target, // meta de la campaña
-                new Date(form.deadline).getTime(), // fecha de finalizacion de la campaña
-                form.image,
-            ])
+            const data = await createCampaign({
+                args: [
+                    address, // dueño de la campaña
+                    form.title, // titulo de la campaña
+                    form.description, // descripcion de la campaña
+                    form.target, // meta de la campaña
+                    new Date(form.deadline).getTime(), // fecha de finalizacion de la campaña
+                    form.image,
+                ]// args de la funcion createCampaign
+            })
 
             console.log("Llamada a la funcion createCampaign realizada con exito", data)
 
